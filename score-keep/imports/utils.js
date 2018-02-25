0f28f99f69163eee3e3d78c3e3baf6d1678d8ec3
Brandon <brandon@reddirtwebdesign.com>
2018-02-25 12:18:57 +0000

    Importing and Exporting

---
+++ score-keep/imports/utils.js
@@ -0,0 +1,7 @@
+console.log('Log from /imports/utils.js')
+
+export const greetUser = function(){
+  return 'Hello User!'
+}
+
+export const name = 'Brandon Smith'
--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -1,1 +1,8 @@
+//import './../imports/utils.js'
+import { greetUser } from './../imports/utils'
+import { add } from './../imports/math'
+
+
+
 console.log('Log From /server/main.js')
+console.log(add(8,7))
