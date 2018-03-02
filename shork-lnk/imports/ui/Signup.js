2dc3c8efbf8f3de84c4b3ac35c5420ac5c7b9614
Brandon <brandon@reddirtwebdesign.com>
2018-03-02 13:39:27 +0000

    Creating User Accounts

--- shork-lnk/imports/ui/Signup.js
+++ shork-lnk/imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Accounts } from 'meteor/accounts-base'
 import { Link } from 'react-router'
 
 export default class Signup extends React.Component{
@@ -13,6 +14,16 @@ export default class Signup extends React.Component{
   
   onSubmit(e){
     e.preventDefault()
+    
+    const email = this.refs.email.value.trim()
+    const password = this.refs.password.value.trim()
+    
+    Accounts.createUser({
+      email,
+      password
+    }, err => {
+      console.log('Signup Callback', e)
+    })
     
     this.setState({
       error: 'Something Went Wrong'
@@ -30,8 +41,8 @@ export default class Signup extends React.Component{
         {this.state.error ? <p>{this.state.error}</p> : undefined}
         
         <form>
-          <input type="email" name="email" placeholder="Email"/>
-          <input type="password" name="password" placeholder="Password"/>
+          <input type="email" ref="email" name="email" placeholder="Email"/>
+          <input type="password" ref="password" name="password" placeholder="Password"/>
           <button onClick={this.onSubmit.bind(this)}>
             Create Account          
           </button>
