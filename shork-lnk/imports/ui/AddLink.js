e2be910d00972c6698543d2e9b4771152cac0a4a
Brandon <brandon@reddirtwebdesign.com>
2018-03-04 13:12:07 +0000

    Refactoring into smaller components

---
+++ shork-lnk/imports/ui/AddLink.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Meteor } from 'meteor/meteor'
+
+export default class AddLink extends React.Component {
+  
+  onSubmit(e){
+    e.preventDefault()
+    
+    const url = this.refs.url.value.trim()
+    
+    if(url){
+      Meteor.call('links.insert', url)
+      
+      this.refs.url.value = ''
+    }
+  }
+  
+  render(){
+    return (
+      <div>
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="url" placeholder="URL"/>
+          <button>Add Link</button>
+        </form>
+      </div>
+    )
+  }
+}
--- shork-lnk/imports/ui/Link.js
+++ shork-lnk/imports/ui/Link.js
@@ -1,42 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Meteor } from 'meteor/meteor'
 import { Accounts } from 'meteor/accounts-base'
 import { Links } from '../api/links'
 
+import PrivateHeader from './PrivateHeader'
+import AddLink from './AddLink'
 import LinksList from './LinksList'
 
 export default class Link extends React.Component {
   
-  onLogout(){
-    Accounts.logout()
-  }
-  
-  onSubmit(e){
-    e.preventDefault()
-    
-    const url = this.refs.url.value.trim()
-    
-    if(url){
-      Meteor.call('links.insert', url)
-      
-      this.refs.url.value = ''
-    }
-  }
-  
   render(){
     return (
       <div>
-        <h1>Your Links</h1>  
-        <button onClick={this.onLogout.bind(this)}>
-          Logout
-        </button>
+        <PrivateHeader title='Your Links'/>
         <LinksList/>
-        <p>Add Link</p>
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL"/>
-          <button>Add Link</button>
-        </form>
+        <AddLink/>
       </div>
     )
   }
