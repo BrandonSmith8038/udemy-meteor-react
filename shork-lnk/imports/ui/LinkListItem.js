17ec90164bb9a6b9ae88c18ed4a71c89c574729e
Brandon <brandon@reddirtwebdesign.com>
2018-03-15 10:48:13 +0000

    Copying links with Clipboard.js

--- shork-lnk/imports/ui/LinkListItem.js
+++ shork-lnk/imports/ui/LinkListItem.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Clipboard from 'clipboard'
 
 export default class LinkListItem extends React.Component {
+  
+  componentDidMount(){
+    this.clipboard = new Clipboard(this.refs.copy)
+    
+    this.clipboard.on('success', () => {
+      alert('It Worked')
+    }).on('error', () => {
+      alert('Unable To Copy, Please Manualy Copy The Link')
+    })
+  }
+  
+  componentWillUnmount(){
+    this.clipboard.destroy
+  }
   
   render() {
     return(
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>Copy</button>
       </div>
     )
   }
