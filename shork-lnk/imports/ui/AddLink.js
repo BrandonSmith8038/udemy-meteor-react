17004e34528ca5a543d5d624335557656976e310
Brandon <brandon@reddirtwebdesign.com>
2018-03-16 12:29:09 +0000

    Modal Styling

--- shork-lnk/imports/ui/AddLink.js
+++ shork-lnk/imports/ui/AddLink.js
@@ -51,10 +51,12 @@ export default class AddLink extends React.Component {
           contentLabel="Add Link"
           onAfterOpen={() => this.refs.url.focus()}
           onRequestClose={this.handleModaleClose.bind(this)}
+          className="boxed-view__box"
+          overlayClassName="boxed-view boxed-view--modal"
         >
         <h1>Add Link</h1>
         {this.state.error ? <p>{this.state.error}</p> : ''}
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={this.onSubmit.bind(this)} className="boxed-view__form">
           <input 
             type="text" 
             ref="url"
@@ -62,9 +64,9 @@ export default class AddLink extends React.Component {
             value={this.state.url}
             onChange={this.onChange.bind(this)}
           />
-          <button>Add Link</button>
+          <button className="button">Add Link</button>
+        <button type="button" className="button button--secondary" onClick={this.handleModaleClose.bind(this)}>Cancel</button>
         </form>
-        <button onClick={this.handleModaleClose.bind(this)}>Cancel</button>
         </Modal>
       </div>
     )
