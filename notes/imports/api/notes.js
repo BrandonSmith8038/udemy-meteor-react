8106b10e85f7af394d9ac765d481fa9cb07b7bd4
Brandon <brandon@reddirtwebdesign.com>
2018-03-22 11:46:28 +0000

    Notes.remove Method and Tests

--- notes/imports/api/notes.js
+++ notes/imports/api/notes.js
@@ -1,6 +1,7 @@
 import { Mongo } from 'meteor/mongo'
 import { Meteor } from 'meteor/meteor'
 import moment from 'moment'
+import SimpleSchema from 'simpl-schema'
 
 export const Notes = new Mongo.Collection('notes')
 
@@ -15,5 +16,18 @@ Meteor.methods({
       userId: this.userId,
       updatedAt: moment().valueOf()
     })
+  },
+  'notes.remove'(_id){
+    if(!this.userId){
+      throw new Meteor.Error('Not-Authorized')
+    }
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      }
+    }).validate({ _id })
+    
+    Notes.remove({ _id, userId: this.userId })
   }
 })
--- notes/imports/api/notes.test.js
+++ notes/imports/api/notes.test.js
@@ -6,6 +6,19 @@ if(Meteor.isServer){
   
   describe('Notes', function () {
     
+    beforeEach(function() {
+      Notes.remove({})
+      
+      Notes.insert(
+        {
+          _id: 'testNoteId1',
+          title: 'My Title',
+          body: 'My body for note',
+          updatedAt: 0,
+          userId: 'testUserId1'
+        }
+      )
+    })
     
     it('Should insert new note', function() {
       const userId = 'testId'
@@ -20,6 +33,21 @@ if(Meteor.isServer){
       }).toThrow()
     })
     
+    it('Should remove note', function() {
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'testUserId1' }, ['testNoteId1'])
+      
+      expect(Notes.findOne({ _id: 'testNoteId1' })).toBeUndefined()
+    })
+    it('Should not remove not if unauthenticated', function(){
+      
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({}, ['testNoteId1'])
+      }).toThrow()
+    })
+    it('Should not remove note if invalid ID', function() {
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({userId: 'testUserId1'}, [])
+      }).toThrow()
+    })
   })
-  
 }
