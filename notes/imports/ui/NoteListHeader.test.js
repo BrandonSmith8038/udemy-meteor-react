2815ed06a6ad225ad46f36c011cee6370ebb3b79
Brandon <brandon@reddirtwebdesign.com>
2018-03-25 11:51:59 +0000

    test: NoteListHeader Tests
    Tests for: Should call meteorCall on click and Should not session for a faild insert

--- notes/imports/ui/NoteListHeader.test.js
+++ notes/imports/ui/NoteListHeader.test.js
@@ -3,16 +3,40 @@ import expect from 'expect'
 import { mount } from 'enzyme'
 import { Meteor } from 'meteor/meteor'
 import { NoteListHeader } from './NoteListHeader'
+import { notes } from '../fixtures/fixtures'
 
 if(Meteor.isClient){
+  
+  let meteorCall
+  let Session
+  
+  beforeEach(function () {
+    meteorCall = expect.createSpy()
+    Session = {
+      set: expect.createSpy()
+    }
+  })
+  
   describe('NoteListHeader', function(){
     it('Should call meteorCall on click', function(){
-      const spy = expect.createSpy()
-      const wrapper = mount(<NoteListHeader meteorCall={spy}/>)
+      
+      const wrapper = mount(<NoteListHeader Session={Session} meteorCall={meteorCall}/>)
       
       wrapper.find('button').simulate('click')
+      meteorCall.calls[0].arguments[1](undefined, notes[0]._id)
       
-      expect(spy).toHaveBeenCalledWith('notes.insert')
+      expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert')
+      expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id)
+    })
+    
+    it('Should not set session for failed inser', function() {
+      const wrapper = mount(<NoteListHeader Session={Session} meteorCall={meteorCall}/>)
+      
+      wrapper.find('button').simulate('click')
+      meteorCall.calls[0].arguments[1]({})
+      
+      expect(meteorCall.calls[0].arguments[0]).toBe('notes.insert')
+      expect(Session.set).toNotHaveBeenCalled()
     })
     
     
