6824c7aa3e7f68546f9706776b9965726ae3d7ef
Brandon <brandon@reddirtwebdesign.com>
2018-03-28 20:17:48 +0000

    feat: Mobile Nav
    Movile Nav functionality and styling

--- notes/imports/ui/PrivateHeader.js
+++ notes/imports/ui/PrivateHeader.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createContainer } from 'meteor/react-meteor-data'
+import { Session } from 'meteor/session'
 
 export const PrivateHeader = (props) => {
+  const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg'
+  
   return (
     <div className="header">
       <div className="header__content">
+        <img 
+          className="header__nav-toggle" 
+          src={navImageSrc} 
+          onClick={props.handleMenuClick} 
+        />
         <h1 className="header__title">{props.title}</h1>  
         <button className="button button--link-text" onClick={() => props.handleLogout()}>
           Logout
@@ -17,11 +25,18 @@ export const PrivateHeader = (props) => {
 
 PrivateHeader.propType = {
   title: React.PropTypes.string.isRequired,
-  handleLogout: React.PropTypes.func.isRequired
+  handleLogout: React.PropTypes.func.isRequired,
+  handleMenuClick: React.PropTypes.func.isRequired,
+  isNavOpen: React.PropTypes.bool.isRequired
 }
 
 export default createContainer(() => {
   return {
-    handleLogout: () => Accounts.logout()
+    handleMenuClick: () => {
+      const menuState = Session.get('isNavOpen')
+      Session.set('isNavOpen', !menuState)
+    },
+    handleLogout: () => Accounts.logout(),
+    isNavOpen: Session.get('isNavOpen')
   }
 }, PrivateHeader)
